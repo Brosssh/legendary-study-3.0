06a4e7a25333f40345928ce103d4e4401e143779
brosssh <tiabroch@gmail.com>
2025-01-22 15:37:27 +0100

    Custome logger

--- app.py
+++ app.py
@@ -8,7 +8,20 @@ import backend.errors
 load_dotenv()
 
 logger = logging.getLogger()
-logging.basicConfig(level=logging.INFO)
+logger.setLevel(logging.INFO)
+
+# create console handler and set level to debug
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+
+# create formatter
+formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
+
+# add formatter to ch
+ch.setFormatter(formatter)
+
+# add ch to logger
+logger.addHandler(ch)
 
 #Initialize clusters
 mongo_manager.MongoUserCluster()
